54587190075c1c14b086c06470abfe83282143ce
liyinqing <yinqing_521@126.com>
2017-05-31 14:04:49 +0800

    上面布局完成

---
+++ app/src/main/java/atguigu/com/exer321/view/Title.java
@@ -0,0 +1,14 @@
+package atguigu.com.exer321.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * 作者：李银庆 on 2017/5/31 14:00
+ */
+public class Title extends LinearLayout{
+    public Title(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
